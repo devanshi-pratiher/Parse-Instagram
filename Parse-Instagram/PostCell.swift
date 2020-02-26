d77689618b798b2cf12ee85eae3b717648047cf9
Devanshi Pratiher <devanshi.pratiher@zineone.com>
2020-02-25 21:41:02 -0500

    Finished Parsetargram Part 1

---
+++ Parse-Instagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Parse-Instagram
+//
+//  Created by Devanshi Pratiher on 2/23/20.
+//  Copyright © 2020 Devanshi Pratiher. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
